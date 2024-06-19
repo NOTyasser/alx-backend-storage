c9cd48c53e98bd439385ca1e62fd0488cb1b934c
NOTyasser <125504642+NOTyasser@users.noreply.github.com>
2024-06-20 01:10:37 +0200

    all

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,58 +1,38 @@
 #!/usr/bin/env python3
-"""
-0x02-redis_basic
-"""
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
-from typing import Callable
+import requests
 from functools import wraps
-from time import sleep
+from typing import Callable
 
 
-redisInstance = redis.Redis()
-redisInstance.flushdb()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def callsCount(method: Callable) -> Callable:
-    """A decorator that takes a method callable argument
-    that increments the callsCount."""
-
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def incrCount(url: str) -> str:
-        """A method that increments the numbers of requests
-        done for a URL."""
-
-        countKey = f"count:{url}"
-        storageKey = f"storage:{url}"
-        storageVal = redisInstance.get(storageKey)
-        if storageVal:
-            redisInstance.incr(countKey)
-            return storageVal.decode("utf-8")
-        call = method(url)
-        redisInstance.setex(storageKey, 10, call)
-        redisInstance.set(countKey, 1)
-        return call
-
-    return incrCount
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
-@callsCount
+@data_cacher
 def get_page(url: str) -> str:
-    """A method that uses the requests module to obtain the HTML
-    content of a particular URL and returns it."""
-    try:
-        response = requests.get(url).text
-        return response
-    except requests.RequestException as e:
-        return
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    print(redisInstance.get(f"storage:{url}"))
-    sleep(12)
-    print(redisInstance.get(f"storage:{url}"))
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
